5a0375e27661440784c392e3f436567016a5bf49
Aniruddh Sharma <aniruddhsharmajpr@gmail.com>
2021-07-16 18:01:00 +0530

    modified app.js

--- app.js
+++ app.js
@@ -12,9 +12,12 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
-  useNewUrlParser: true,
-});
+mongoose.connect(
+  "mongodb+srv://<username>:1452@cluster0.9iebs.mongodb.net/todolistDB",
+  {
+    useNewUrlParser: true,
+  }
+);
 
 const itemsSchema = {
   name: String,
