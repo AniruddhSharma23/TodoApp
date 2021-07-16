b70230d1eb11f2e84a3d3569f8fd755fe246721b
Aniruddh Sharma <aniruddhsharmajpr@gmail.com>
2021-07-16 18:02:29 +0530

    modified app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose.connect(
-  "mongodb+srv://<username>:1452@cluster0.9iebs.mongodb.net/todolistDB",
+  "mongodb+srv://ani:1452@cluster0.9iebs.mongodb.net/todolistDB",
   {
     useNewUrlParser: true,
   }
